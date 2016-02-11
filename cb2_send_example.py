d02eb2bc7dbdd3339707bb8b7870ab5dcceb754f
Michael Sobrepera <mjsobrep@live.com>
2016-02-11 14:05:58 -0500

    cleaned up cb2_send_example

--- cb2_send_example.py
+++ cb2_send_example.py
@@ -1,16 +1,24 @@
+"""This basic script demonstrates usage of the cb2_send module."""
+
 import cb2_send
+import socket
 import time
 
 
 HOST = "192.168.1.100"    # The remote host
 PORT = 30003              # The same port as used by the server
+robot_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+robot_socket.connect((HOST, PORT))
 
-robot = cb2_send.URSender(HOST, PORT, True)
+robot = cb2_send.URSender(robot_socket, True)
 
-angleStart = [90, -95, 90, 0, 90, 90]
-angleStart = map(cb2_send.deg_2_rad, angleStart)
+angle_start = [90, -95, 90, 0, 90, 90]
+angle_start = map(cb2_send.deg_2_rad, angle_start)
 
-robot.move_joint(angleStart)
-print "sleep time"
-time.sleep(5)
-print "done sleeping"
+robot.move_joint(angle_start)
+time.sleep(2.5)  # Need to wait for move to complete
+
+angle_finish = [70, -65, 90, 0, 90, 90]
+angle_finish = map(cb2_send.deg_2_rad, angle_finish)
+
+robot.move_joint(angle_finish)

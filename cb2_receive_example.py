11726e5b0e00827b0306cae7df989464f96d5b2e
Michael Sobrepera <mjsobrep@live.com>
2016-02-11 12:21:22 -0500

    fixed to work with new socket passing methodology

--- cb2_receive_example.py
+++ cb2_receive_example.py
@@ -1,19 +1,22 @@
 import cb2_receive
 import time
+import socket
 
 HOST = "192.168.1.100"    # The remote host
 PORT = 30003              # The same port as used by the server
 
-my_ur_receiver = cb2_receive.URReceiver(HOST, PORT, True)
+_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+_socket.connect((HOST, PORT))
+my_ur_receiver = cb2_receive.URReceiver(_socket, True)
 
 my_ur_receiver.start()
 
-some_num = 0
+# some_num = 0
 
 try:
     while True:
-        print "\n\n" + str(some_num) + "\n\n"
-        some_num += 1
+        # print "\n\n" + str(some_num) + "\n\n"
+        # some_num += 1
         time.sleep(.25)
 except KeyboardInterrupt:
     my_ur_receiver.stop()

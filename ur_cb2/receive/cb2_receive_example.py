60df10a7b6b6b5e60bfd6a133d15d82ad6c93ca3
Michael Sobrepera <mjsobrep@live.com>
2016-02-14 14:06:12 -0500

    added argument support

--- ur_cb2/receive/cb2_receive_example.py
+++ ur_cb2/receive/cb2_receive_example.py
@@ -10,11 +10,28 @@ import socket
 import time
 from contextlib import closing
 import cb2_receive
+import argparse
 
 
 def main():
-    host = "192.168.1.100"    # The remote host
-    port = 30003              # The same port as used by the server
+    # Parse in arguments
+    parser = argparse.ArgumentParser(
+        description='Save Points',
+        epilog="This software is designed to show the status of a UR CB2 Robot."
+               " Simply run the program with appropriate arguments and it will"
+               " print useful information about the robot to the terminal."
+               " If information is not printed nicely, make your terminal "
+               "larger")
+
+    parser.add_argument("--ip", metavar="ip", type=str,
+                        help='IP address of the robot', default="192.168.1.100")
+
+    parser.add_argument("--port", metavar="port", type=int,
+                        help='IP port on the robot', default=30003)
+
+    args = parser.parse_args()
+    host = args.ip    # The remote host
+    port = args.port  # The same port as used by the server
 
     with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM))\
             as robot_socket:

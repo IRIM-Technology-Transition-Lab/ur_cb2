e0e5e3616b8cd696869cb672c337604842bc64b9
Michael Sobrepera <mjsobrep@live.com>
2016-03-21 23:46:49 -0400

    corrected wrong flag for default

--- docs/conf.py
+++ docs/conf.py
@@ -127,11 +127,12 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if on_rtd:
-    html_theme = '/default.css'
-else:
-    html_theme = 'default'
+# on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+# if on_rtd:
+#     html_theme = '/default.css'
+# else:
+html_theme = 'default'
+html_style = '/default.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
